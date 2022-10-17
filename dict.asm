da035d6e85fe8f09fe1f1c9d1c301cb39756f458
Alan <tyujn24@mail.ru>
2022-10-17 13:45:30 +0300

    Caller saved

--- dict.asm
+++ dict.asm
@@ -11,7 +11,15 @@ section .text
 global find_word
 find_word:
     add rsi, 8
+    push rax
+    push rcx
+    push r10
+    push r11
     call string_equals
+    pop r11
+    pop r10
+    pop rcx
+    pop rax
     sub rsi, 8
     test rax, rax
     jnz .ret
