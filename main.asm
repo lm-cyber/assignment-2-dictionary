759ea735681190831d1bd8e4ae72fc5a11c904b4
Alan <tyujn24@mail.ru>
2022-10-17 13:40:02 +0300

    Changed sections and added inc files

--- main.asm
+++ main.asm
@@ -1,19 +1,17 @@
 %define BUFFER_SIZE 256
 
-extern read_char
-extern print_string
-extern print_error
-extern string_length
-extern print_newline
-extern find_word
-extern exit
+%include 'lib.inc'
+%include 'dict.inc'
 
-section .data
+section .rodata
 %include 'words.inc'
 initial_prompt_msg: db "Key to find in dictionary:", 0xA, 0
 error_not_found_msg: db "The key you are looking for was not found in the dictionary", 0xA, 0
 error_overflow_msg: db "The query you typed is longer than 255 characters", 0xA, 0
 seperator: db " : ", 0
+
+section .bss
+buffer: resb BUFFER_SIZE
 
 section .text
 
@@ -23,7 +21,6 @@ _start:
     call print_string
 
     xor rcx, rcx                 ; Char count
-    sub rsp, BUFFER_SIZE    ; Reserve space for input
 .read_chars:
     push rcx
     call read_char
@@ -36,7 +33,7 @@ _start:
 
     cmp rcx, BUFFER_SIZE-1     ; Check for overflow
     jge .overflow_error
-    mov [rsp+rcx], al
+    mov [buffer+rcx], al
     inc rcx
     jmp .read_chars
 .overflow_error:
@@ -44,9 +41,9 @@ _start:
     call print_error
     jmp .exit
 .terminate_input:
-    mov byte [rsp+rcx], 0
+    mov byte [buffer+rcx], 0
 .find_in_dict:
-    mov rdi, rsp
+    mov rdi, buffer
     mov rsi, dict_head
 
     call find_word
@@ -71,6 +68,5 @@ _start:
     mov rdi, error_not_found_msg
     call print_error
 .exit:
-    add rsp, BUFFER_SIZE
     xor rdi, rdi
     call exit
--- makefile
+++ makefile
@@ -2,7 +2,7 @@
 
 words.inc: colon.inc
 
-%.o: %.asm words.inc colon.inc
+%.o: %.asm words.inc colon.inc lib.inc dict.inc
 	nasm -felf64 -g -o $@ $<
 
 main: main.o lib.o dict.o
